dd93c4edd24dd63621fa3da62a20a822d911f683
certara-jcraig <61294078+certara-jcraig@users.noreply.github.com>
2021-12-07 12:20:43 -0800

    add table functions

--- R/table.R
+++ R/table.R
@@ -35,6 +35,59 @@ theme_pps_table <- function(x, font_name = "Times New Roman", font_size = 10){
     x <- flextable::align(x = x, i = 1, j = c(-1), align = "center", part = "header")
     x <- flextable::set_table_properties(x=x,layout = "autofit", width = 1)
     x
-  }
+}
+
+
+#' Table function for scm
+#'
+#' @param scmobject
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+tabscm <- function(scmobject,...){
+
+  scmobject$scmlog %>%
+    #mutate(LRT=ifelse(is.na(LRT),"-",table1::round_pad(LRT,3))) %>%
+    #mutate(Deviance=ifelse(is.na(Deviance),"-",table1::round_pad(Deviance,3))) %>%
+
+    {if ("LRT" %in% names(scmobject$scmlog)) dplyr::mutate(., LRT=ifelse(is.na(LRT),"-",table1::round_pad(LRT,3))) else . } %>%
+    {if ("Deviance" %in% names(scmobject$scmlog)) dplyr::mutate(., Deviance=ifelse(is.na(Deviance),"-",table1::round_pad(Deviance,3))) else . } %>%
+    {if ("rss" %in% names(scmobject$scmlog)) dplyr::mutate(.,rss=ifelse(is.na(rss),"-",table1::round_pad(rss,3))) else .} %>%
+    {if ("sumsq" %in% names(scmobject$scmlog)) dplyr::mutate(., sumsq=ifelse(is.na(sumsq),"-",table1::round_pad(sumsq,3))) else . } %>%
+    {if ("df" %in% names(scmobject$scmlog)) dplyr::mutate(., df=ifelse(is.na(df),"-",as.character(df))) else . } %>%
+    {if ("AIC" %in% names(scmobject$scmlog)) dplyr::mutate(., AIC=ifelse(is.na(AIC),"-",table1::round_pad(AIC,3))) else . } %>%
+
+    {if ("LRT" %in% names(scmobject$scmlog)) dplyr::rename(., `Delta -2LL`=LRT) else . } %>%
+    {if ("LRT" %in% names(scmobject$scmlog) & "Deviance" %in% names(scmobject$scmlog)) dplyr::rename(., `-2LL`=Deviance) else . } %>%
+    {if ("Deviance" %in% names(scmobject$scmlog) & !("LRT" %in% names(scmobject$scmlog))) dplyr::rename(., `Delta -2LL`=Deviance) else . } %>%
+    {if ("rss" %in% names(scmobject$scmlog)) dplyr::rename(.,SS=rss) else .} %>%
+    {if ("sumsq" %in% names(scmobject$scmlog)) dplyr::rename(., SUMsq=sumsq) else . } %>%
+
+    {if ("AIC" %in% scmobject$test) dplyr::select(.,-c('p.value')) else . } %>%
+    {if ("Chisq" %in% scmobject$test)  dplyr::mutate(.,p.value=dplyr::case_when(is.na(p.value)~"-",
+                                                                                p.value<0.001 ~'<0.001',
+                                                                                TRUE ~ table1::signif_pad(p.value,3))) else . }  %>%
+
+
+    {if ("select" %in% names(scmobject$scmlog)) dplyr::mutate(.,select=dplyr::case_when(term=='reference' ~ "-",
+                                                                                        select==1 ~ 'Yes',
+                                                                                        select==0 ~ 'No',
+                                                                                        TRUE ~ '??')) else . } %>%
+
+    flextable::as_grouped_data(groups = c("direction"))%>%
+    flextable::as_flextable()%>%
+    #as_flextable(hide_grouplabel = TRUE)%>%
+    flextable::compose(i = ~ !is.na(direction), # when var_group not NA
+                       j = "term", # on column "var"
+                       # create a paragraph containing a chunk containing value of `var_group`
+                       value = flextable::as_paragraph(flextable::as_chunk(direction))) %>%
+    flextable::bold(j = 1, i = ~ !is.na(direction), bold = TRUE, part = "body" ) %>%
+    flextable::align( i = ~ is.na(direction), j = c(-1), align = "center", part = "body")   %>%
+    {if ("Chisq" %in% scmobject$test) flextable::compose(.,j = 'p.value',i= 1,part="header",value = flextable::as_paragraph(flextable::as_i("p"),glue::glue(" value"))) else .} %>%
+
+    flextable::set_header_labels(term=" ")
+}
 
 
